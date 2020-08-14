a6706af6e63ca8c78a4987e2e408638ab028a76d
Bruno Grillo <21182200+putzgrillo@users.noreply.github.com>
2020-08-14 18:55:20 -0300

    Create L2Q2.R

---
+++ MSc_Computacional/L2Q2.R
@@ -0,0 +1,35 @@
+  # QUESTÃO 2: REGRESSÃO NÃO-LINEAR ----
+      # Q2: A: ----
+df2 <- within(data.frame(x = runif(n = 200, min = 2, max = 40)), {y = 60 * exp(-0.05 * x) + rnorm(n = 200)})
+
+      # Q2: B: ----
+            # FUNÇÃO OBJETIVO
+funcaoOtimizar <- function(parametros, X, Y) {Y - parametros[1] * exp(parametros[2] * X)}  # f = y - b0* exp(b1*x)
+            # JACOBIANA FUNÇÃO OBJETIVO (RESÍDUOS)
+jacobianaFuncaoOtimizar <- function(parametros, X, Y) {  
+  lista <- list(
+    db0 = -exp(parametros[2] * X),                                # df/db0 = -exp(b1*x)
+    db1 =  -parametros[1] * exp(parametros[2] * X) * X            # df/db1 = -b0* exp(b1*x) * x
+  )
+  do.call(cbind, lista)
+}  
+            # GAUSS-NEWTON
+gaussNewtonR <- function(f, J, X, Y, chuteInicial, tolerancia = 0.00001, iteracaoMaxima = 500) {
+  etapa <- 0
+  x0 <- chuteInicial
+  valoresParciais <- vector("list", iteracaoMaxima)
+  variacao <- tolerancia + 1
+  while( sum(variacao > tolerancia) > 0 ) {
+    # b_(t+1) = b_(t) - (J^T * J)^(-1) * J^T * epsilon(f(b_(t)))
+    x1 <- x0 - solve(t(J(parametros = x0, X = X, Y = Y)) %*% J(parametros = x0, X = X, Y = Y)) %*% t(J(parametros = x0, X = X, Y = Y)) %*% f(parametros = x0, X = X, Y = Y)
+    # x1 <- x0 - MASS::ginv(t(J(parametros =x1 <- x0 - solve(t(J(parametros = x0, X = X, Y = Y)) %*% J(parametros = x0, X = X, Y = Y)) %*% t(J(parametros = x0, X = X, Y = Y)) %*% f(parametros = x0, X = X, Y = Y) x0, X = X, Y = Y)) %*% J(parametros = x0, X = X, Y = Y)) %*% t(J(parametros = x0, X = X, Y = Y)) %*% f(parametros = x0, X = X, Y = Y)
+    etapa <- etapa + 1
+    variacao <<-  abs(x1 - x0)
+    x0 <- x1
+    valoresParciais[[etapa]] <- x1
+    if (etapa == iteracaoMaxima) {break}
+  }
+  resultado <- list(nIteracoes = etapa, parametros = matrix(x1, ncol = 1))
+return(resultado)
+}
+
