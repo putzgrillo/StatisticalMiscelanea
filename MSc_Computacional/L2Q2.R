b3a3271040fc1482259a1db174570e8ec6503f98
Bruno Grillo <21182200+putzgrillo@users.noreply.github.com>
2020-08-16 20:15:11 -0300

    Update L2Q2.R

--- MSc_Computacional/L2Q2.R
+++ MSc_Computacional/L2Q2.R
@@ -25,7 +25,7 @@ return(resultado)
 }
 
 
-# AVALIAÇÃO
+                # AVALIAÇÃO
 funcaoOtimizar <- function(parametros, X, Y) {Y - parametros[1] * exp(parametros[2] * X)}  # f = y - b0* exp(b1*x)
 jacobianaFuncaoOtimizar <- function(parametros, X, Y) {  
   lista <- list(
@@ -44,3 +44,39 @@ dfChutes <- vector("list", nrow(chutes))
 for (w in seq_along(dfChutes)) {
   dfChutes[[w]] <- gaussNewtonR(f = funcaoOtimizar, J = jacobianaFuncaoOtimizar, X = df2$x, Y = df2$y, chuteInicial = as.numeric(chutes[w,]))
 }
+
+      # Q2: C ----
+              # APROVEITANDO O CÓDIGO DA LISTA 1
+newtonRaphson <- function(f, g, chuteInicial, X, Y, erro = 0.00001, iteracaoMaxima = 5000, ...) {
+  etapa <- 0
+  x0 <- chuteInicial
+  while( sum(abs(f(x0, X = X, Y = Y)) > erro) > 0 ) {
+    x1 <- x0 - solve(g(x0, X = X, Y = Y)) %*% f(x0, X = X, Y = Y)
+    etapa <- etapa + 1
+    x0 <- x1
+    if (etapa == iteracaoMaxima) {break}
+  }
+  resultado <- list(nIteracoes = etapa, raizes = matrix(x1, ncol = 1))
+  return(resultado)
+}
+
+            # APLICAR NEWTON-RAPHSON
+fNR <- function(parametros, X, Y) {
+  c(
+    -2*sum((Y - parametros[1]*exp(parametros[2]*X))*exp(parametros[2]*X)), 
+    -2*sum((Y - parametros[1]*exp(parametros[2]*X))*X*parametros[1]*exp(parametros[2]*X))
+  )
+}
+
+gNR <- function(parametros, X, Y) {
+  matrix(c(
+    sum(-2*exp(parametros[2]*X)*(-exp(parametros[2]*X))),
+    sum(2*(-parametros[1]*X*exp(parametros[2]*X))*(-exp(parametros[2]*X))) + sum(2*(Y-parametros[1]*exp(parametros[2]*X))*(-X*exp(parametros[2]*X))),
+    sum(2*(-parametros[1]*X*exp(parametros[2]*X))*(-exp(parametros[2]*X))) + sum(2*(Y-parametros[1]*exp(parametros[2]*X))*(-X*exp(parametros[2]*X))),
+    sum(2*(-parametros[1]*X*exp(parametros[2]*X))*(-parametros[1]*X*exp(parametros[2]*X))) + sum(2*(Y-parametros[1]*exp(parametros[2]*X))*(-parametros[1]*X^2*exp(parametros[2]*X)))
+  ), ncol = 2, byrow = TRUE)
+}
+  
+
+newtonRaphson(f = fNR, g = gNR, X = df2$x, Y = df2$y, chuteInicial = c(55, -0.05))
+newtonRaphson(f = fNR, g = gNR, X = df2$x, Y = df2$y, chuteInicial = c(0, -0.05))
